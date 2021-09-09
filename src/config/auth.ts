da4714c8039271a31dfab5bb9c5962beadfed0be
Thiago Alvares <thiago.cesaralvares@gmail.com>
2021-09-08 22:30:24 -0300

    Testes de autenticaçao de usuario finalizado

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 export default {
   jwt: {
     secret: process.env.JWT_SECRET as string,
---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,70 @@
+import { create } from "domain";
+import { emitWarning } from "process";
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { ICreateUserDTO } from "../createUser/ICreateUserDTO";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+import { IncorrectEmailOrPasswordError } from "./IncorrectEmailOrPasswordError";
+
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let usersRepository: IUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+describe("Authenticate User", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepository);
+    authenticateUserUseCase = new AuthenticateUserUseCase(usersRepository);
+  });
+
+  it("should be able to authenticate an user", async () => {
+    const user: ICreateUserDTO = {
+      email: "test@jest.com",
+      name: "Test Jest",
+      password: "test"
+    };
+
+    await createUserUseCase.execute(user);
+
+    const result = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password
+    });
+
+    expect(result).toHaveProperty("token");
+  });
+
+  it("should not be able to authenticate with an nonexistent user", () => {
+    expect(async () => {
+      const user: ICreateUserDTO = {
+        email: "test@jest.com",
+        name: "Test Jest",
+        password: "test"
+      };
+
+      await createUserUseCase.execute(user);
+
+      await authenticateUserUseCase.execute({
+        email: "test@error.com",
+        password: user.password
+      });
+    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError);
+  });
+
+  it("should not be able to authenticate with incorrect password", () => {
+    expect(async () => {
+      const user: ICreateUserDTO = {
+        email: "test@jest.com",
+        name: "Test Jest",
+        password: "test"
+      };
+
+      await createUserUseCase.execute(user);
+
+      await authenticateUserUseCase.execute({
+        email: user.email,
+        password: "testerror"
+      });
+    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError);
+  });
+});
